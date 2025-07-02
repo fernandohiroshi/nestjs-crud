3c71aa929f11a7de711dfe012e16d971711dec65
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2025-07-02 06:39:08 -0300

    feat: create users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MessagesModule } from './messages/messages.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
+  imports: [MessagesModule, UsersModule],
   controllers: [],
   providers: [],
 })
