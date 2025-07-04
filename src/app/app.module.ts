f18e9dd7a21179686109862871de06172d401112
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2025-07-04 08:07:49 -0300

    feat: add filters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,18 @@ import { MessagesModule } from '../messages/messages.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from '../users/users.module';
 import { SimpleMiddleware } from './common/middlewares/simple.middleware';
+import { APP_FILTER } from '@nestjs/core';
+import { MyExceptionFilter } from './common/filters/my-exception.filter';
 
 @Module({
   imports: [MessagesModule, UsersModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: MyExceptionFilter,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
---
+++ src/app/common/filters/my-exception.filter.ts
@@ -0,0 +1,32 @@
+import {
+  ArgumentsHost,
+  BadRequestException,
+  Catch,
+  ExceptionFilter,
+} from '@nestjs/common';
+
+@Catch(BadRequestException)
+export class MyExceptionFilter<T extends BadRequestException>
+  implements ExceptionFilter
+{
+  catch(exception: T, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const response = context.getResponse();
+
+    const statusCode = exception.getStatus();
+    const exceptionResponse = exception.getResponse();
+
+    const err =
+      typeof response === 'string'
+        ? {
+            message: exceptionResponse,
+          }
+        : (exceptionResponse as object);
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    response.status(statusCode).json({
+      ...err,
+    });
+  }
+}
